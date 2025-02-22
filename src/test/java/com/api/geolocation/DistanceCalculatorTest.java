c70169a13da80c52f58c278834f9f8e078f300e6
Leonardo Herrera <leonardoherrera2304@gmail.com>
2025-02-21 23:24:44 -0400

    feat: distance calculator implemented

---
+++ src/test/java/com/api/geolocation/DistanceCalculatorTest.java
@@ -0,0 +1,18 @@
+package com.api.geolocation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DistanceCalculatorTest {
+    @Test
+    public void testDistance() {
+        double lat1 = 40.7128;
+        double lon1 = -74.0060;
+        double lat2 = 34.0522;
+        double lon2 = -118.2437;
+
+        double distance = DistanceCalculator.calcularDistancia(lat1, lon1, lat2, lon2);
+        assertEquals(3935.746254609722, distance);
+    }
+}
