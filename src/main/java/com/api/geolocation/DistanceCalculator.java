cc5c8390297805dabdb06b2d5705320c9d5ac85d
Leonardo Herrera <leonardoherrera2304@gmail.com>
2025-02-21 23:29:01 -0400

    haversine's formula

--- src/main/java/com/api/geolocation/DistanceCalculator.java
+++ src/main/java/com/api/geolocation/DistanceCalculator.java
@@ -3,7 +3,7 @@ package com.api.geolocation;
 public class DistanceCalculator {
     private static final double RADIO_TIERRA_KM = 6371.0;
 
-    public static double calcularDistancia(double lat1, double lon1, double lat2, double lon2) {
+    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
         double lat1Rad = Math.toRadians(lat1);
         double lon1Rad = Math.toRadians(lon1);
         double lat2Rad = Math.toRadians(lat2);
@@ -12,6 +12,7 @@ public class DistanceCalculator {
         double deltaLat = lat2Rad - lat1Rad;
         double deltaLon = lon2Rad - lon1Rad;
 
+        //Haversine's formula
         double a = Math.pow(Math.sin(deltaLat / 2), 2) +
                 Math.cos(lat1Rad) * Math.cos(lat2Rad) *
                         Math.pow(Math.sin(deltaLon / 2), 2);
--- src/test/java/com/api/geolocation/DistanceCalculatorTest.java
+++ src/test/java/com/api/geolocation/DistanceCalculatorTest.java
@@ -12,7 +12,7 @@ public class DistanceCalculatorTest {
         double lat2 = 34.0522;
         double lon2 = -118.2437;
 
-        double distance = DistanceCalculator.calcularDistancia(lat1, lon1, lat2, lon2);
+        double distance = DistanceCalculator.calculateDistance(lat1, lon1, lat2, lon2);
         assertEquals(3935.746254609722, distance);
     }
 }
