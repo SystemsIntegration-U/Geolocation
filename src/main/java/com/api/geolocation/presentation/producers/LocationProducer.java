57912e7fa9ddf41d83f6e93667f3e7cfb1e980e6
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2025-02-28 19:08:50 -0400

    feat: implemented producer to send nearest branches found

--- src/main/java/com/api/geolocation/presentation/producers/LocationProducer.java
+++ src/main/java/com/api/geolocation/presentation/producers/LocationProducer.java
@@ -1,20 +1,49 @@
 package com.api.geolocation.presentation.producers;
 
+import com.api.geolocation.application.transaction.BranchDTO;
+import com.api.geolocation.application.transaction.BranchesWithMedicineDTO;
+import com.api.geolocation.application.transaction.LocationDTO;
+import com.api.geolocation.application.transaction.RequiredMedicineDTO;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import static com.api.geolocation.infrastructure.config.queue.RabbitMQConstantHandler.TOPIC_EXCHANGE;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Service
+import static com.api.geolocation.infrastructure.config.queue.RabbitMQConstants.BRANCHES_WITH_MEDICINE_KEY;
+import static com.api.geolocation.infrastructure.config.queue.RabbitMQConstants.MEDICINE_EXCHANGE;
+
+@Component
 @AllArgsConstructor
-public class CoordinatesProducer {
+@Slf4j
+public class LocationProducer {
 
     private RabbitTemplate rabbitTemplate;
 
-    public String sendCoordinates(double latitude, double longitude, String medicineId) {
-        String message = latitude + "," + longitude + "," + medicineId;
-        rabbitTemplate.convertAndSend(TOPIC_EXCHANGE, "route.coordinates", message);
-        return "Coordinates sent: " + message;
+    public void sendBranchesWithMedicine(List<BranchDTO> nearestBranches, RequiredMedicineDTO requiredMedicineDTO) {
+        try {
+            List<LocationDTO> nearbyPoints = nearestBranches.stream()
+                    .map(BranchDTO::getLocation)
+                    .collect(Collectors.toList());
+
+            BranchesWithMedicineDTO branchesWithMedicineDTO = new BranchesWithMedicineDTO(
+                    requiredMedicineDTO.getOriginCoordinates(),
+                    nearbyPoints,
+                    requiredMedicineDTO.getProductDetails(),
+                    requiredMedicineDTO.getRange()
+            );
+
+            rabbitTemplate.convertAndSend(
+                    MEDICINE_EXCHANGE,
+                    BRANCHES_WITH_MEDICINE_KEY,
+                    branchesWithMedicineDTO
+            );
+            log.info("sendBranchesWithMedicine: (arrayLength: {}, medicine: {}, medicineStock: {})",
+                    nearestBranches.size(), requiredMedicineDTO.getProductDetails().getId(), requiredMedicineDTO.getProductDetails().getStock());
+        } catch (Exception exception) {
+            log.warn(exception.getMessage());
+        }
     }
 }
